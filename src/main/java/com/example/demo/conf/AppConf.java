90af2c038c26b63df7ba23c5e711c7673d022003
unknown <cmouli00@gmail.com>
2020-07-19 17:47:53 +0530

    new commit

--- src/main/java/com/example/demo/conf/AppConf.java
+++ src/main/java/com/example/demo/conf/AppConf.java
@@ -1,8 +1,12 @@
 package com.example.demo.conf;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@Profile("dev")
 public class AppConf implements WebMvcConfigurer {
 
     @Override
