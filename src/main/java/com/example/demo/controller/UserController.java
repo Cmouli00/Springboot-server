0fd950234e368eca1ae4fb136b772873ea93abf4
unknown <cmouli00@gmail.com>
2020-07-19 16:59:32 +0530

    first commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @RestController
 public class UserController {
 
